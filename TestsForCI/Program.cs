d1c9461990ddf10b2a3e9a8ee0147ee1ffb25cce
dzhyr <Dmytro.Zhyrko@henryschein.com>
2021-06-07 13:23:49 +0300

    Add tests

---
+++ TestsForCI/Program.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace TestsForCI
+{
+    [TestFixture]
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var actual = Sum(2, 3);
+            Assert.AreEqual(actual, 5);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var actual = Sum(4, 5);
+            Assert.AreEqual(actual, 9);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            var actual = Sum(10, 3);
+            Assert.AreEqual(actual, 14);
+        }
+    }
+}
